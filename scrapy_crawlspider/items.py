3d9205c00461488592e77c57c38fcdb6a280529e
Christopher Yohan Chung <Christopher Yohan Chung>
2017-02-09 19:17:31 +1100

    Creating item class and applying

--- scrapy_crawlspider/items.py
+++ scrapy_crawlspider/items.py
@@ -6,9 +6,8 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.item import Item, Field
 
 
-class ScrapyCrawlspiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class WikiItem(scrapy.Item):
+    name = Field()
--- scrapy_crawlspider/spiders/crawlspider.py
+++ scrapy_crawlspider/spiders/crawlspider.py
@@ -1,6 +1,6 @@
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
-
+from scrapy_crawlspider.items import WikiItem
 
 class WikiSpider(CrawlSpider):
 
@@ -12,6 +12,7 @@ class WikiSpider(CrawlSpider):
         Rule(LinkExtractor(allow="https://en.wikipedia.org/wiki/", restrict_xpaths="//div[@class='mw-body']//a"), callback='parse_page', follow=False),
     )
 
-    def parse_page(self, response):        
-        print response.xpath('//h1[@class="firstHeading"]/text()').extract()
-        return
+    def parse_page(self, response):                
+        item = WikiItem()
+        item["name"] = response.xpath('//h1[@class="firstHeading"]/text()').extract()
+        return item
